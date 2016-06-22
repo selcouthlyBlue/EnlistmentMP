27c17e7930db5d582a6e879c909f085af612dc7e
Jerome <j.patrick.wiz@gmail.com>
2016-06-22 13:00:08 +0800

    Removed unnecessary test case in EnlistServiceTest

--- src/test/java/com/orangeandbronze/enlistment/service/EnlistServiceTest.java
+++ src/test/java/com/orangeandbronze/enlistment/service/EnlistServiceTest.java
@@ -58,34 +58,4 @@ public class EnlistServiceTest {
 		assertTrue(section.contains(student));
 		verify(studentSemEnlistmentsDao).update(studentSemEnlistments);
 	}
-	
-	@Test
-	public void enlistingACurrentlyEnlistedSectionRaisesAnException() throws Exception {
-		final int studentNumber = 12345;
-		final Student student = new Student(studentNumber);
-		when(studentDao.findBy(studentNumber)).thenReturn(student);
-		
-		final Subject subjectCommonToBothSections = new Subject("ML100");
-		final Room room = Room.RH300;
-		final String sectionID = "MEH1";
-		final Schedule schedule = new Schedule(Days.MTH, Period.FIRSTPERIOD);
-		final Section section = new Section(sectionID, schedule, room, subjectCommonToBothSections);
-		when(sectionDao.findBy(sectionID)).thenReturn(section);
-		
-		final Schedule differentSchedule = new Schedule(Days.TF, Period.SECONDPERIOD);
-		final String differentSectionID = "TFC1";
-		final Section sectionWithSameSubjectAsAnotherSection = new Section(differentSectionID, differentSchedule, room, subjectCommonToBothSections);
-		when(sectionDao.findBy(differentSectionID)).thenReturn(sectionWithSameSubjectAsAnotherSection);
-		
-		service.enlist(studentNumber, sectionID);
-		try {
-			service.enlist(studentNumber, differentSectionID);
-			fail("enlist() method of Student object "
-					+ "should thorw an exception if the section being enlisted "
-					+ "has the same subject as any of the previously enlisted "
-					+ "sections.");
-		} catch (ConflictInSectionEnlistmentException expected) {
-			
-		}
-	}
 }
